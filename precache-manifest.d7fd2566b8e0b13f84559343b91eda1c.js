7bb64bc88a17180f7df4a5b86a25d55f0da52d53
Harpaul Gill <harpaul_gill@hotmail.com>
2019-02-10 22:30:35 -0800

    Updates

---
+++ precache-manifest.d7fd2566b8e0b13f84559343b91eda1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea3d82363d6e54d87d30",
+    "url": "/MoniteringClient/static/js/runtime~main.ea3d8236.js"
+  },
+  {
+    "revision": "6d60eaf2f6a1294f72ac",
+    "url": "/MoniteringClient/static/js/main.6d60eaf2.chunk.js"
+  },
+  {
+    "revision": "c33a8e0ef25b241f0d6c",
+    "url": "/MoniteringClient/static/js/1.c33a8e0e.chunk.js"
+  },
+  {
+    "revision": "6d60eaf2f6a1294f72ac",
+    "url": "/MoniteringClient/static/css/main.8c3b743f.chunk.css"
+  },
+  {
+    "revision": "ee96bf1adf6094ec7a541c6e282c8c80",
+    "url": "/MoniteringClient/index.html"
+  }
+];
